3d2898cba76b3085884874f89423d2ebfb8e48b4
rakibkhanofficial <khanrakib218@gmail.com>
2024-05-20 20:28:52 +0600

    feat: Initial Commit

---
+++ src/components/feature/Footer/index.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Button, Link } from "@nextui-org/react";
+import { FaCcVisa, FaCcMastercard, FaPaypal } from "react-icons/fa6";
+import { TiArrowRepeatOutline } from "react-icons/ti";
+
+const FooterComponent = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="bg-gray-900 py-4 text-gray-300">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
+          {/* Schems */}
+          <div className="flex flex-col items-center justify-center gap-6 lg:flex-col">
+            <Link>
+              <h1 className="cursor-pointer text-xl font-bold text-gray-300">
+                Schems
+              </h1>
+            </Link>
+            <p>Corporate</p>
+            <p>Business</p>
+            <p>Consulting</p>
+          </div>
+
+          {/* Services */}
+          <div className="flex flex-col items-center justify-center gap-6 lg:flex-col">
+            <Link>
+              <h1 className="cursor-pointer text-xl font-bold text-gray-300">
+                Services
+              </h1>
+            </Link>
+            <p>Services</p>
+            <p>Flat Booking</p>
+            <p>Construction</p>
+          </div>
+
+          {/* Contact */}
+          <div className="flex flex-col items-center justify-center gap-6 lg:flex-col">
+            <Link>
+              <h1 className=" cursor-pointer text-xl font-bold text-gray-300 ">
+                Contact
+              </h1>
+            </Link>
+            <p>Rampura,Dhaka</p>
+            <p>+88012345-67891</p>
+            <p>statebuilder@builderstate.com</p>
+          </div>
+
+          {/* Newsletter */}
+          <div className=" flex flex-col items-center justify-center gap-6 lg:flex-col">
+            <h1 className="text-xl font-bold lg:text-center">Newsletter</h1>
+            <div className="relative max-w-xs sm:max-w-md">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                className=" w-full rounded-md border-gray-300 px-10 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 lg:rounded"
+              />
+              <Button
+                color="primary"
+                type="submit"
+                className="absolute right-0 top-0 h-full"
+              >
+                Subscribe
+              </Button>
+            </div>
+            <div className="mt-2 flex justify-center gap-2">
+              <FaCcVisa className="h-10 w-10 sm:h-12 sm:w-12 lg:h-14 lg:w-14" />
+              <FaCcMastercard className="h-10 w-10 sm:h-12 sm:w-12 lg:h-14 lg:w-14" />
+              <FaPaypal className="h-10 w-10 sm:h-12 sm:w-12 lg:h-14 lg:w-14" />
+            </div>
+          </div>
+        </div>
+        <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+        <div className=" mx-auto grid grid-cols-1 gap-6 px-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 ">
+          <div className=" flex items-center justify-center gap-1 ">
+            <TiArrowRepeatOutline />
+
+            <h1>Builder State</h1>
+          </div>
+          <div className=" flex items-center justify-center gap-1 ">
+            <h1>&copy;{currentYear} </h1>
+            <p>Privacy</p>
+            <p>Terms & Conditions</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterComponent;
---
+++ src/components/feature/Header/index.tsx
@@ -0,0 +1,132 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from "@nextui-org/react";
+import {
+  ChevronDown,
+  Lock,
+  Activity,
+  Flash,
+  Server,
+  TagUser,
+  Scale,
+} from "./icons";
+
+const HomeComponent = () => {
+  const icons = {
+    chevron: <ChevronDown fill="currentColor" size={16} />,
+    scale: <Scale className="text-warning" fill="currentColor" size={30} />,
+    lock: <Lock className="text-success" fill="currentColor" size={30} />,
+    activity: (
+      <Activity className="text-secondary" fill="currentColor" size={30} />
+    ),
+    flash: <Flash className="text-primary" fill="currentColor" size={30} />,
+    server: <Server className="text-success" fill="currentColor" size={30} />,
+    user: <TagUser className="text-danger" fill="currentColor" size={30} />,
+  };
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Builder State</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="bg-transparent p-0 data-[hover=true]:bg-transparent"
+                endContent={icons.chevron}
+                radius="sm"
+                variant="light"
+              >
+                Features
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            className="w-[340px]"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem
+              key="autoscaling"
+              description="ACME scales apps to meet user demand, automagically, based on load."
+              startContent={icons.scale}
+            >
+              Autoscaling
+            </DropdownItem>
+            <DropdownItem
+              key="usage_metrics"
+              description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+              startContent={icons.activity}
+            >
+              Usage Metrics
+            </DropdownItem>
+            <DropdownItem
+              key="production_ready"
+              description="ACME runs on ACME, join us and others serving requests at web scale."
+              startContent={icons.flash}
+            >
+              Production Ready
+            </DropdownItem>
+            <DropdownItem
+              key="99_uptime"
+              description="Applications stay on the grid with high availability and high uptime guarantees."
+              startContent={icons.server}
+            >
+              +99% Uptime
+            </DropdownItem>
+            <DropdownItem
+              key="supreme_support"
+              description="Overcome any challenge with a supporting team ready to respond."
+              startContent={icons.user}
+            >
+              +Supreme Support
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarItem>
+          <Link href="#" color="foreground">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About Us
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="/login">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          {/* signup button here  */}
+          <Button as={Link} color="primary" href="/register" variant="flat">
+            Register
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default HomeComponent;
