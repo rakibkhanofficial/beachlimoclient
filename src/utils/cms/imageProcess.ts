ed51b8724e825b3a45343a83cc90034351c7662c
rakibkhanofficial <khanrakib218@gmail.com>
2024-05-29 01:00:48 +0600

    fix: fisher

--- src/utils/cms/imageProcess.ts
+++
@@ -1,6 +0,0 @@
-import { sanityClient } from './sanity'
-import createImageUrlBuilder from '@sanity/image-url'
-
-export const urlForThumbnail = (source: string) => {
-  return createImageUrlBuilder(sanityClient).image(source).url()
-}
--- src/utils/cms/videoProcess.ts
+++
@@ -1,6 +0,0 @@
-import { getFileAsset } from '@sanity/asset-utils'
-import { config } from './sanity'
-
-export function videoAssetFor(source: string) {
-  return getFileAsset(source, config)
-}
