8c80331d8cddd6d0298e8945f6e3668ae9e99f92
rakibkhanofficial <khanrakib218@gmail.com>
2024-05-29 17:52:59 +0600

    fix: fix animation

--- src/components/feature/CityToCityServices/step4/index.tsx
+++ src/components/feature/CityToCityServices/step4/index.tsx
@@ -1,40 +1,41 @@
-import React, { useEffect, useRef } from "react";
-import lottie, { type AnimationItem } from "lottie-web"; // Import AnimationItem
-import Successlottie from "../Success.json";
+import React from "react";
+// import lottie, { type AnimationItem } from "lottie-web"; // Import AnimationItem
+// import Successlottie from "../Success.json";
 import { useRouter } from "next/navigation";
 import { Button } from "@nextui-org/react";
 import { useAppDispatch } from "~@/_redux/hooks/hooks";
 import { handleCitytocityStepNext } from "~@/modules/citotocityservice/_redux/actions/citytocityActions";
+import CarAnimation from "./CarAnimation";
 
 const CarBookingBycitytovitySucessFull = () => {
-  const animationContainer = useRef<HTMLDivElement>(null);
-  const animationInstance = useRef<AnimationItem | null>(null); // Adjust the type here
-  const parentContainer = useRef<HTMLDivElement>(null);
+  // const animationContainer = useRef<HTMLDivElement>(null);
+  // const animationInstance = useRef<AnimationItem | null>(null); // Adjust the type here
+  // const parentContainer = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    animationInstance.current = lottie.loadAnimation({
-      container: animationContainer.current!,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: Successlottie,
-      // Add width and height properties here
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice", // Adjust this according to your needs
-        progressiveLoad: true,
-      },
-      // Adjust the width and height values as per your requirement
-    });
+  // useEffect(() => {
+  //   animationInstance.current = lottie.loadAnimation({
+  //     container: animationContainer.current!,
+  //     renderer: "svg",
+  //     loop: true,
+  //     autoplay: true,
+  //     animationData: Successlottie,
+  //     // Add width and height properties here
+  //     rendererSettings: {
+  //       preserveAspectRatio: "xMidYMid slice", // Adjust this according to your needs
+  //       progressiveLoad: true,
+  //     },
+  //     // Adjust the width and height values as per your requirement
+  //   });
 
-    return () => {
-      // Cleanup function to stop animation when component unmounts
-      if (animationInstance.current) {
-        animationInstance.current.destroy();
-      }
-    };
-  }, []);
+  //   return () => {
+  //     // Cleanup function to stop animation when component unmounts
+  //     if (animationInstance.current) {
+  //       animationInstance.current.destroy();
+  //     }
+  //   };
+  // }, []);
 
   const handleNext = () => {
     router.push("/");
@@ -52,18 +53,16 @@ const CarBookingBycitytovitySucessFull = () => {
         <Divider />
       </div> */}
       <div className=" flex min-h-[65vh] flex-col items-center justify-center ">
-        <div className="flex h-[150px] w-[150px] items-center justify-center ">
-          <div ref={parentContainer}>
-            <div ref={animationContainer}></div>
-          </div>
+        <div className="h-[250px] w-[350] lg:w-[450px]">
+        <CarAnimation/>
         </div>
         <div className=" flex flex-col items-center justify-center ">
           <p className=" py-2 text-center text-base font-semibold text-gray-600 dark:text-gray-200 ">
-            Rent Create Successfull
+            Car Booking Successfull
           </p>
           <p className=" w-[80%] pb-2 text-center text-sm text-gray-500 dark:text-gray-200 ">
-            Congratulations Your Car Rent has been created succesfull. Click on
-            next button to continue
+            Congratulations Your Car Booking has been created succesfully. Click on
+             button to continue
           </p>
         </div>
         <div className="flex flex-col items-center justify-center py-2 ">
