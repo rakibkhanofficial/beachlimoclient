3219000ecc9dff92c22e13803ccc9022df0786f6
rakibkhanofficial <khanrakib218@gmail.com>
2024-06-08 22:30:01 +0600

    add route change

--- src/utils/api/route.ts
+++ src/utils/api/route.ts
@@ -2,16 +2,20 @@ export const endPoints = {
   auth: {
     // sendOtp: 'auth/sendOtp',
     // verifyOtp: 'auth/verifyOtp',
-    register: 'users/signup',
-    login: 'users/login',
+    register: 'auth/signup',
+    login: 'auth/login',
     updatePassword: 'auth/update-password',
-    logout: 'users/logout',
+    logout: 'auth/logout',
   },
   Customer: {
-    CreateBooking: "carrents/createCarRental",
-    getRentAllByuserId: (userId: string) => `carrents/rentalbyuser/${userId}`,
-    getPendingRentAllByuserId: (userId: string) => `carrents/pendingrentalbyuser/${userId}`,
-    getRentadetailsbyRentId: "carrents/rentalbyid"
+    CreateBooking: "customer/createCarRental",
+    getRentAllByuserId: (userId: string) => `customer/rentalbyuser/${userId}`,
+    getPendingRentAllByuserId: (userId: string) => `customer/pendingrentalbyuser/${userId}`,
+    getAcceptedRentAllByuserId: (userId: string) => `customer/acceptedbookingbyuserid/${userId}`,
+    getAssignRentAllByuserId: (userId: string) => `customer/assignbookingbyuserid/${userId}`,
+    getCompleteRentAllByuserId: (userId: string) => `customer/completebookingbyuserid/${userId}`,
+    getCancelRentAllByuserId: (userId: string) => `customer/cancelbookingbyuserid/${userId}`,
+    getRentadetailsbyRentId: "customer/rentalbyid"
   },
   Admin: {
     getAllCustomerList: "admin/allusersdata",
